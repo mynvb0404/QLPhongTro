986462042a92487b3a279cbd86ddfba13c2971df
mynvb0404 <ngovubaomy9914@gmail.com>
2026-05-23 22:44:51 +0700

    Gán sự kiện cho các nút: Hóa đơn, báo cáo, tin ở ghép

--- GUI_QuanLy/frmMenu.Designer.cs
+++ GUI_QuanLy/frmMenu.Designer.cs
@@ -76,48 +76,43 @@
             pnSideBar.Controls.Add(label1);
             pnSideBar.Dock = DockStyle.Left;
             pnSideBar.Location = new Point(0, 0);
-            pnSideBar.Margin = new Padding(5);
             pnSideBar.Name = "pnSideBar";
-            pnSideBar.Size = new Size(408, 877);
+            pnSideBar.Size = new Size(251, 548);
             pnSideBar.TabIndex = 0;
             // 
             // panel3
             // 
             panel3.Controls.Add(lbEmail);
             panel3.Controls.Add(lbFullName);
-            panel3.Location = new Point(0, 699);
-            panel3.Margin = new Padding(5);
+            panel3.Location = new Point(0, 437);
             panel3.Name = "panel3";
-            panel3.Size = new Size(408, 112);
+            panel3.Size = new Size(251, 70);
             panel3.TabIndex = 2;
             // 
             // lbEmail
             // 
             lbEmail.ForeColor = SystemColors.ControlDark;
-            lbEmail.Location = new Point(20, 54);
-            lbEmail.Margin = new Padding(5, 0, 5, 0);
+            lbEmail.Location = new Point(12, 34);
             lbEmail.Name = "lbEmail";
-            lbEmail.Size = new Size(372, 48);
+            lbEmail.Size = new Size(229, 30);
             lbEmail.TabIndex = 1;
             lbEmail.Text = "Email";
             // 
             // lbFullName
             // 
             lbFullName.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 163);
-            lbFullName.Location = new Point(20, 6);
-            lbFullName.Margin = new Padding(5, 0, 5, 0);
+            lbFullName.Location = new Point(12, 4);
             lbFullName.Name = "lbFullName";
-            lbFullName.Size = new Size(384, 48);
+            lbFullName.Size = new Size(236, 30);
             lbFullName.TabIndex = 0;
             lbFullName.Text = "Tên người dùng";
             // 
             // label3
             // 
             label3.BorderStyle = BorderStyle.Fixed3D;
-            label3.Location = new Point(0, 699);
-            label3.Margin = new Padding(5, 0, 5, 0);
+            label3.Location = new Point(0, 437);
             label3.Name = "label3";
-            label3.Size = new Size(406, 3);
+            label3.Size = new Size(250, 2);
             label3.TabIndex = 1;
             // 
             // panel1
@@ -125,10 +120,9 @@
             panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             panel1.Controls.Add(panel2);
             panel1.Controls.Add(label5);
-            panel1.Location = new Point(0, 706);
-            panel1.Margin = new Padding(5);
+            panel1.Location = new Point(0, 441);
             panel1.Name = "panel1";
-            panel1.Size = new Size(406, 184);
+            panel1.Size = new Size(250, 115);
             panel1.TabIndex = 0;
             // 
             // panel2
@@ -136,20 +130,18 @@
             panel2.Controls.Add(pictureBox1);
             panel2.Controls.Add(btLogout);
             panel2.Dock = DockStyle.Bottom;
-            panel2.Location = new Point(0, 115);
-            panel2.Margin = new Padding(5);
+            panel2.Location = new Point(0, 72);
             panel2.Name = "panel2";
-            panel2.Size = new Size(406, 69);
+            panel2.Size = new Size(250, 43);
             panel2.TabIndex = 5;
             // 
             // pictureBox1
             // 
             pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             pictureBox1.Image = Properties.Resources.logout;
-            pictureBox1.Location = new Point(104, 10);
-            pictureBox1.Margin = new Padding(5);
+            pictureBox1.Location = new Point(64, 6);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(39, 45);
+            pictureBox1.Size = new Size(24, 28);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -161,10 +153,9 @@
             btLogout.FlatStyle = FlatStyle.Flat;
             btLogout.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 163);
             btLogout.ForeColor = Color.Red;
-            btLogout.Location = new Point(153, 8);
-            btLogout.Margin = new Padding(5);
+            btLogout.Location = new Point(94, 5);
             btLogout.Name = "btLogout";
-            btLogout.Size = new Size(140, 50);
+            btLogout.Size = new Size(86, 31);
             btLogout.TabIndex = 4;
             btLogout.Text = "Đăng xuất";
             btLogout.TextAlign = ContentAlignment.MiddleRight;
@@ -174,44 +165,42 @@
             // label5
             // 
             label5.BorderStyle = BorderStyle.Fixed3D;
-            label5.Location = new Point(0, 123);
-            label5.Margin = new Padding(5, 0, 5, 0);
+            label5.Location = new Point(0, 77);
             label5.Name = "label5";
-            label5.Size = new Size(406, 3);
+            label5.Size = new Size(250, 2);
             label5.TabIndex = 2;
             // 
             // btReport
             // 
             btReport.FlatAppearance.BorderSize = 0;
             btReport.FlatStyle = FlatStyle.Flat;
-            btReport.Location = new Point(20, 619);
-            btReport.Margin = new Padding(5);
+            btReport.Location = new Point(12, 387);
             btReport.Name = "btReport";
-            btReport.Size = new Size(377, 50);
+            btReport.Size = new Size(232, 31);
             btReport.TabIndex = 8;
             btReport.Text = "Báo cáo thống kê";
             btReport.UseVisualStyleBackColor = true;
+            btReport.Click += btReport_Click;
             // 
             // btBill
             // 
             btBill.FlatAppearance.BorderSize = 0;
             btBill.FlatStyle = FlatStyle.Flat;
-            btBill.Location = new Point(20, 560);
-            btBill.Margin = new Padding(5);
+            btBill.Location = new Point(12, 350);
             btBill.Name = "btBill";
-            btBill.Size = new Size(377, 50);
+            btBill.Size = new Size(232, 31);
             btBill.TabIndex = 7;
             btBill.Text = "Hóa đơn và thanh toán";
             btBill.UseVisualStyleBackColor = true;
+            btBill.Click += btBill_Click;
             // 
             // btContract
             // 
             btContract.FlatAppearance.BorderSize = 0;
             btContract.FlatStyle = FlatStyle.Flat;
-            btContract.Location = new Point(20, 501);
-            btContract.Margin = new Padding(5);
+            btContract.Location = new Point(12, 313);
             btContract.Name = "btContract";
-            btContract.Size = new Size(377, 50);
+            btContract.Size = new Size(232, 31);
             btContract.TabIndex = 6;
             btContract.Text = "Quản lý hợp đồng";
             btContract.UseVisualStyleBackColor = true;
@@ -221,22 +210,21 @@
             // 
             bPost.FlatAppearance.BorderSize = 0;
             bPost.FlatStyle = FlatStyle.Flat;
-            bPost.Location = new Point(20, 442);
-            bPost.Margin = new Padding(5);
+            bPost.Location = new Point(12, 276);
             bPost.Name = "bPost";
-            bPost.Size = new Size(377, 50);
+            bPost.Size = new Size(232, 31);
             bPost.TabIndex = 5;
             bPost.Text = "Tin ở ghép";
             bPost.UseVisualStyleBackColor = true;
+            bPost.Click += bPost_Click;
             // 
             // btResident
             // 
             btResident.FlatAppearance.BorderSize = 0;
             btResident.FlatStyle = FlatStyle.Flat;
-            btResident.Location = new Point(20, 382);
-            btResident.Margin = new Padding(5);
+            btResident.Location = new Point(12, 239);
             btResident.Name = "btResident";
-            btResident.Size = new Size(377, 50);
+            btResident.Size = new Size(232, 31);
             btResident.TabIndex = 4;
             btResident.Text = "Quản lý khách thuê";
             btResident.UseVisualStyleBackColor = true;
@@ -246,10 +234,9 @@
             // 
             btAppointment.FlatAppearance.BorderSize = 0;
             btAppointment.FlatStyle = FlatStyle.Flat;
-            btAppointment.Location = new Point(20, 323);
-            btAppointment.Margin = new Padding(5);
+            btAppointment.Location = new Point(12, 202);
             btAppointment.Name = "btAppointment";
-            btAppointment.Size = new Size(377, 50);
+            btAppointment.Size = new Size(232, 31);
             btAppointment.TabIndex = 3;
             btAppointment.Text = "Quản lý lịch hẹn";
             btAppointment.UseVisualStyleBackColor = true;
@@ -259,10 +246,9 @@
             // 
             btRoom.FlatAppearance.BorderSize = 0;
             btRoom.FlatStyle = FlatStyle.Flat;
-            btRoom.Location = new Point(20, 264);
-            btRoom.Margin = new Padding(5);
+            btRoom.Location = new Point(12, 165);
             btRoom.Name = "btRoom";
-            btRoom.Size = new Size(377, 50);
+            btRoom.Size = new Size(232, 31);
             btRoom.TabIndex = 2;
             btRoom.Text = "Quản lý phòng";
             btRoom.UseVisualStyleBackColor = true;
@@ -272,10 +258,9 @@
             // 
             btArea.FlatAppearance.BorderSize = 0;
             btArea.FlatStyle = FlatStyle.Flat;
-            btArea.Location = new Point(20, 205);
-            btArea.Margin = new Padding(5);
+            btArea.Location = new Point(12, 128);
             btArea.Name = "btArea";
-            btArea.Size = new Size(377, 50);
+            btArea.Size = new Size(232, 31);
             btArea.TabIndex = 1;
             btArea.Text = "Quản lý khu vực";
             btArea.UseVisualStyleBackColor = true;
@@ -286,10 +271,9 @@
             btAcc.FlatAppearance.BorderSize = 0;
             btAcc.FlatStyle = FlatStyle.Flat;
             btAcc.ImageAlign = ContentAlignment.MiddleLeft;
-            btAcc.Location = new Point(20, 146);
-            btAcc.Margin = new Padding(5);
+            btAcc.Location = new Point(12, 91);
             btAcc.Name = "btAcc";
-            btAcc.Size = new Size(377, 50);
+            btAcc.Size = new Size(232, 31);
             btAcc.TabIndex = 0;
             btAcc.Text = "Quản lý tài khoản";
             btAcc.UseVisualStyleBackColor = true;
@@ -298,19 +282,17 @@
             // label2
             // 
             label2.BorderStyle = BorderStyle.Fixed3D;
-            label2.Location = new Point(5, 94);
-            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Location = new Point(3, 59);
             label2.Name = "label2";
-            label2.Size = new Size(406, 3);
+            label2.Size = new Size(250, 2);
             label2.TabIndex = 0;
             // 
             // label1
             // 
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
             label1.Location = new Point(0, 0);
-            label1.Margin = new Padding(5, 0, 5, 0);
             label1.Name = "label1";
-            label1.Size = new Size(406, 94);
+            label1.Size = new Size(250, 59);
             label1.TabIndex = 0;
             label1.Text = "HỆ THỐNG QUẢN LÝ PHÒNG TRỌ";
             label1.TextAlign = ContentAlignment.TopCenter;
@@ -318,21 +300,19 @@
             // pnMain
             // 
             pnMain.Dock = DockStyle.Fill;
-            pnMain.Location = new Point(408, 0);
-            pnMain.Margin = new Padding(5);
+            pnMain.Location = new Point(251, 0);
             pnMain.Name = "pnMain";
-            pnMain.Size = new Size(1545, 877);
+            pnMain.Size = new Size(933, 548);
             pnMain.TabIndex = 1;
             // 
             // frmMenu
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1953, 877);
+            ClientSize = new Size(1184, 548);
             Controls.Add(pnMain);
             Controls.Add(pnSideBar);
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(5);
             Name = "frmMenu";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Trang chủ";
